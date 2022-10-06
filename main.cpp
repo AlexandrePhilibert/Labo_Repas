11cb9e234853866fb339e65c9d3741795c7e7f75
Alexandre Philibert <alexandre.philibert@ik.me>
2022-10-06 17:58:26 +0200

    Add define

--- main.cpp
+++ main.cpp
@@ -1,45 +1,47 @@
 // PRG1_E
-//---------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------
 // Demo           : Labo_02_D
 // Fichier        : Labo_02_D.cpp
 // Auteur(s)      : BEE Gianni & PHILIBERT Alexandre
-// But            : Calcule le prix d'un repas en fonction d'une commande
+// But            : Calcul du prix d'un repas en fonction d'une commande entrée par l'utilisateur
 // Modifications  :
-// Remarque(s)    : Les valeurs de saisi sont contrôlée dans l'intervale [0 .. 10]
-                    Les
-//---------------------------------------------------------------------------------
+// Remarque(s)    : Les valeurs de saisi sont contrôlée dans l'intervalle [0,10]
+//                  Les autres erreurs de saisi ne sont pas gérée
+// -----------------------------------------------------------------------------------------------
 
 #include <iostream>     // cin et cout
-#include <cstdlib>      // EXIT_SUCCESS
+#include <cstdlib>      // EXIT_SUCCESS, EXIT_FAILURE
 #include <iomanip>      // setprecision
 #include <limits>       // numeric_limits<streamsize>
+
+#define EMPTY_BUFFER cin.ignore(numeric_limits<streamsize>::max(), '\n');
+#define CLEAN_EXIT cout << "Pressez ENTER pour quitter"; \
+                   EMPTY_BUFFER
 
 using namespace std;    // Simplification std::cin <=> cin ; std::cout <=> cin
 
 int main() {
-   const float    PRIX_ENTREE = 4.30f,
-                  PRIX_PLAT_PRINCIPAL = 12.50f,
-                  PRIX_BOISSON = 3.00f,
-                  PRIX_DESSERT = 6.00f,
-                  PRIX_CAFE = 2.80f;
+   const float  PRIX_ENTREE = 4.30f,
+                PRIX_PLAT_PRINCIPAL = 12.50f,
+                PRIX_BOISSON = 3.00f,
+                PRIX_DESSERT = 6.00f,
+                PRIX_CAFE = 2.80f;
 
-   // TODO: Signed ou unsigned ?
-   unsigned int   nombreEntree,
-                  nombrePlat,
-                  nombreBoisson,
-                  nombreDessert,
-                  nombreCafe;
-
+   unsigned int nombreEntree,
+                nombrePlat,
+                nombreBoisson,
+                nombreDessert,
+                nombreCafe;
 
    cout << "Bonjour," << endl
         << "Ce programme calcul le prix d'un repas" << endl << endl
         << "======================================"         << endl
-        << "- Entree  : " << PRIX_ENTREE                     << endl
-        << "- Plat    : " << PRIX_PLAT_PRINCIPAL             << endl
-        << "- Boisson : " << PRIX_BOISSON                    << endl
-        << "- Dessert : " << PRIX_DESSERT                    << endl
-        << "- Cafe    : " << PRIX_CAFE                       << endl
-        << "NB : Limite de saisie [0 - 10]";
+        << "- Entree  : " << PRIX_ENTREE                    << endl
+        << "- Plat    : " << PRIX_PLAT_PRINCIPAL            << endl
+        << "- Boisson : " << PRIX_BOISSON                   << endl
+        << "- Dessert : " << PRIX_DESSERT                   << endl
+        << "- Cafe    : " << PRIX_CAFE                      << endl
+        << "NB : Limite de saisie [0 - 10]"                 << endl;
 
    cout << setprecision(2) << fixed; // Définit l'affichage
 
@@ -50,30 +52,48 @@ int main() {
 
    cout << " - nbre d'PRIX_ENTREE : ";
    cin  >> nombreEntree;
-   cin.ignore(numeric_limits<streamsize>::max(), '\n');
+   EMPTY_BUFFER;
 
    if (nombreEntree < 0 || nombreEntree > 10) {
-      cout << endl;
-      cout << "limite de saisie [0 - 10], votre saisie : " << nombreEntree;
-
+      CLEAN_EXIT;
       return EXIT_FAILURE;
    }
 
    cout << " - nbre de plat : ";
    cin  >> nombrePlat;
-   cin.ignore(numeric_limits<streamsize>::max(), '\n');
+   EMPTY_BUFFER;
+
+   if (nombrePlat < 0 || nombrePlat > 10) {
+      CLEAN_EXIT;
+      return EXIT_FAILURE;
+   }
 
    cout << " - nbre PRIX_BOISSON : ";
    cin  >> nombreBoisson;
-   cin.ignore(numeric_limits<streamsize>::max(), '\n');
+   EMPTY_BUFFER;
+
+   if (nombreBoisson < 0 || nombreBoisson > 10) {
+      CLEAN_EXIT;
+      return EXIT_FAILURE;
+   }
 
    cout << " - nbre de PRIX_DESSERT : ";
    cin  >> nombreDessert;
-   cin.ignore(numeric_limits<streamsize>::max(), '\n');
+   EMPTY_BUFFER;
+
+   if (nombreDessert < 0 || nombreDessert > 10) {
+      CLEAN_EXIT;
+      return EXIT_FAILURE;
+   }
 
    cout << " - nbre de cafe : ";
    cin  >> nombreCafe;
-   cin.ignore(numeric_limits<streamsize>::max(), '\n');
+   EMPTY_BUFFER;
+
+   if (nombreCafe < 0 || nombreCafe > 10) {
+      CLEAN_EXIT;
+      return EXIT_FAILURE;
+   }
 
    cout << endl;
 
@@ -99,9 +119,6 @@ int main() {
 
    cout << endl;
 
-
-   cout << "Pressez ENTER pour quitter";
-   cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
+   CLEAN_EXIT;
    return EXIT_SUCCESS;
 }
