7dd8628af1111bb3d3bdf445eb5b00cffccae889
Alexandre Philibert <alexandre.philibert@ik.me>
2022-10-07 18:18:30 +0200

    Improve TUI

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
-// PRG1_E
 // -----------------------------------------------------------------------------------------------
 // Demo           : Labo_02_D
 // Fichier        : Labo_02_D.cpp
 // Auteur(s)      : BEE Gianni & PHILIBERT Alexandre
-// But            : Calcul du prix d'un repas en fonction d'une commande entrée par l'utilisateur
+// But            : Calcul du prix d'un repas en fonction des éléments choisi parmi le menu
 // Modifications  :
-// Remarque(s)    : Les valeurs de saisi sont contrôlée dans l'intervalle [0,10]
-//                  Les autres erreurs de saisi ne sont pas gérée
+// Remarque(s)    : Les valeurs de saisi sont contrôlée dans l'intervalle [0,10].
+//                  Il est possible pour l'utilisateur de faire déborder les variables d'entrées,
+//                  par ex. -65534 = 2, dans ce cas le programme continue son exécution.
+//                  Les autres erreurs de saisies ne sont pas gérée.
 // -----------------------------------------------------------------------------------------------
 
 #include <iostream>     // cin et cout
@@ -14,90 +15,104 @@
 #include <iomanip>      // setprecision
 #include <limits>       // numeric_limits<streamsize>
 
-#define EMPTY_BUFFER cin.ignore(numeric_limits<streamsize>::max(), '\n');
-#define CLEAN_EXIT cout << "Pressez ENTER pour quitter"; \
-                   EMPTY_BUFFER
+#define VIDER_BUFFER        cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define AFFICHE_MESSAGE_FIN cout << "Pressez ENTER pour quitter"
 
-using namespace std;    // Simplification std::cin <=> cin ; std::cout <=> cin
+using namespace std; // Simplification std::cin <=> cin ; std::cout <=> cin
 
 int main() {
-   const float  PRIX_ENTREE = 4.30f,
-                PRIX_PLAT_PRINCIPAL = 12.50f,
-                PRIX_BOISSON = 3.00f,
-                PRIX_DESSERT = 6.00f,
-                PRIX_CAFE = 2.80f;
+   const unsigned int LARGEUR_COLONNE_TICKET = 9,
+                      LARGEUR_COLONNE_PRIX = 8,
+                      LARGEUR_COLONNE_COMMANDE = 18;
 
-   unsigned int nombreEntree,
-                nombrePlat,
-                nombreBoisson,
-                nombreDessert,
-                nombreCafe;
+   // Les limites de saisies pour les éléments du repas
+   const unsigned int CHOIX_MINIMUM = 0,
+                      CHOIX_MAXIMUM = 10;
 
-   cout << "Bonjour," << endl
-        << "Ce programme calcul le prix d'un repas" << endl << endl
-        << "======================================"         << endl
-        << "- Entree  : " << PRIX_ENTREE                    << endl
-        << "- Plat    : " << PRIX_PLAT_PRINCIPAL            << endl
-        << "- Boisson : " << PRIX_BOISSON                   << endl
-        << "- Dessert : " << PRIX_DESSERT                   << endl
-        << "- Cafe    : " << PRIX_CAFE                      << endl
-        << "NB : Limite de saisie [0 - 10]"                 << endl;
+   const float PRIX_ENTREE = 4.30f,
+               PRIX_PLAT_PRINCIPAL = 12.50f,
+               PRIX_BOISSON = 3.00f,
+               PRIX_DESSERT = 6.00f,
+               PRIX_CAFE = 2.80f;
 
-   cout << setprecision(2) << fixed; // Définit l'affichage
+   // Variables d'entrées utilisateur
+   unsigned short nombreEntree,
+                  nombrePlat,
+                  nombreBoisson,
+                  nombreDessert,
+                  nombreCafe;
 
-   // Commande
+   // Définit la précision d'affichage des nombres à virgules (exemple: 2.50)
+   cout << setprecision(2) << fixed;
+
+   cout << "bonjour, ce programme calcul le prix d'un repas, voici le prix des elements" << endl
+        << "===========================================================================" << endl
+        << left << setw(LARGEUR_COLONNE_TICKET) << "- entree"  << " : " << right << setw(LARGEUR_COLONNE_PRIX) << PRIX_ENTREE         << endl
+        << left << setw(LARGEUR_COLONNE_TICKET) << "- plat"    << " : " << right << setw(LARGEUR_COLONNE_PRIX) << PRIX_PLAT_PRINCIPAL << endl
+        << left << setw(LARGEUR_COLONNE_TICKET) << "- boisson" << " : " << right << setw(LARGEUR_COLONNE_PRIX) << PRIX_BOISSON        << endl
+        << left << setw(LARGEUR_COLONNE_TICKET) << "- dessert" << " : " << right << setw(LARGEUR_COLONNE_PRIX) << PRIX_DESSERT        << endl
+        << left << setw(LARGEUR_COLONNE_TICKET) << "- cafe"    << " : " << right << setw(LARGEUR_COLONNE_PRIX) << PRIX_CAFE           << endl
+        << "NB : limite de saisie [0 - 10]" << endl
+        << endl;
 
    cout << "votre commande" << endl
         << "==============" << endl;
 
-   cout << " - nbre d'PRIX_ENTREE : ";
+   cout << left;
+
+   cout << setw(LARGEUR_COLONNE_COMMANDE) << " - nbre d'entree " << " : ";
    cin  >> nombreEntree;
-   EMPTY_BUFFER;
+   VIDER_BUFFER;
 
-   if (nombreEntree < 0 || nombreEntree > 10) {
-      CLEAN_EXIT;
+   if (nombreEntree < CHOIX_MINIMUM || nombreEntree > CHOIX_MAXIMUM) {
+      AFFICHE_MESSAGE_FIN;
+      VIDER_BUFFER;
       return EXIT_FAILURE;
    }
 
-   cout << " - nbre de plat : ";
+   cout << setw(LARGEUR_COLONNE_COMMANDE) << " - nbre de plat" << " : ";
    cin  >> nombrePlat;
-   EMPTY_BUFFER;
+   VIDER_BUFFER;
 
-   if (nombrePlat < 0 || nombrePlat > 10) {
-      CLEAN_EXIT;
+   if (nombrePlat < CHOIX_MINIMUM || nombrePlat > CHOIX_MAXIMUM) {
+      AFFICHE_MESSAGE_FIN;
+      VIDER_BUFFER;
       return EXIT_FAILURE;
    }
 
-   cout << " - nbre PRIX_BOISSON : ";
+   cout << setw(LARGEUR_COLONNE_COMMANDE) << " - nbre boisson" << " : ";
    cin  >> nombreBoisson;
-   EMPTY_BUFFER;
+   VIDER_BUFFER;
 
-   if (nombreBoisson < 0 || nombreBoisson > 10) {
-      CLEAN_EXIT;
+   if (nombreBoisson < CHOIX_MINIMUM || nombreBoisson > CHOIX_MAXIMUM) {
+      AFFICHE_MESSAGE_FIN;
+      VIDER_BUFFER;
       return EXIT_FAILURE;
    }
 
-   cout << " - nbre de PRIX_DESSERT : ";
+   cout << setw(LARGEUR_COLONNE_COMMANDE) << " - nbre de dessert" << " : ";
    cin  >> nombreDessert;
-   EMPTY_BUFFER;
+   VIDER_BUFFER;
 
-   if (nombreDessert < 0 || nombreDessert > 10) {
-      CLEAN_EXIT;
+   if (nombreDessert < CHOIX_MINIMUM || nombreDessert > CHOIX_MAXIMUM) {
+      AFFICHE_MESSAGE_FIN;
+      VIDER_BUFFER;
       return EXIT_FAILURE;
    }
 
-   cout << " - nbre de cafe : ";
+   cout << setw(LARGEUR_COLONNE_COMMANDE) << " - nbre de cafe" << " : ";
    cin  >> nombreCafe;
-   EMPTY_BUFFER;
+   VIDER_BUFFER;
 
-   if (nombreCafe < 0 || nombreCafe > 10) {
-      CLEAN_EXIT;
+   if (nombreCafe < CHOIX_MINIMUM || nombreCafe > CHOIX_MAXIMUM) {
+      AFFICHE_MESSAGE_FIN;
+      VIDER_BUFFER;
       return EXIT_FAILURE;
    }
 
    cout << endl;
 
-   // Calcul des prix pour chaques plats/boissons
+   // Calcul des totaux pour chaque elements
    float totalEntree        = nombreEntree  * PRIX_ENTREE,
          totalPlatPrincipal = nombrePlat    * PRIX_PLAT_PRINCIPAL,
          totalBoisson       = nombreBoisson * PRIX_BOISSON,
@@ -109,16 +124,16 @@ int main() {
 
    cout << "Votre ticket" << endl
         << "============" << endl
-        << " - " << nombreEntree   << " entrees : "  << totalEntree        << endl
-        << " - " << nombrePlat     << " plats : "    << totalPlatPrincipal << endl
-        << " - " << nombreBoisson  << " boissons : " << totalBoisson       << endl
-        << " - " << nombreDessert  << " desserts : " << totalDessert       << endl
-        << " - " << nombreCafe     << " cafes : "    << totalCafe          << endl
-        << " - total : " << total  << endl
-        << "merci de votre visite" << endl;
+        << " - " << nombreEntree  << left  << setw(LARGEUR_COLONNE_TICKET) << " entrees"  << " : " << right << setw(LARGEUR_COLONNE_PRIX) << totalEntree        << endl
+        << " - " << nombrePlat    << left  << setw(LARGEUR_COLONNE_TICKET) << " plats"    << " : " << right << setw(LARGEUR_COLONNE_PRIX) << totalPlatPrincipal << endl
+        << " - " << nombreBoisson << left  << setw(LARGEUR_COLONNE_TICKET) << " boissons" << " : " << right << setw(LARGEUR_COLONNE_PRIX) << totalBoisson       << endl
+        << " - " << nombreDessert << left  << setw(LARGEUR_COLONNE_TICKET) << " desserts" << " : " << right << setw(LARGEUR_COLONNE_PRIX) << totalDessert       << endl
+        << " - " << nombreCafe    << left  << setw(LARGEUR_COLONNE_TICKET) << " cafes"    << " : " << right << setw(LARGEUR_COLONNE_PRIX) << totalCafe          << endl
+        << " - " << " "           << right << setw(LARGEUR_COLONNE_TICKET) << " total"    << " : " << right << setw(LARGEUR_COLONNE_PRIX) << total              << endl
+        << "merci de votre visite" << endl
+        << endl;
 
-   cout << endl;
-
-   CLEAN_EXIT;
+   AFFICHE_MESSAGE_FIN;
+   VIDER_BUFFER;
    return EXIT_SUCCESS;
 }
